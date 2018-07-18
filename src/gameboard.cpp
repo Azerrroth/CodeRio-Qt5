49e068a8e110b474a09958e40ccb3485d7a717be
Tudero Li <33458031+Youggls@users.noreply.github.com>
2018-07-18 19:47:56 +0800

    实现了从txt文件初始化场景，部分内容未完成
    请将txt文件下载，否则编码有问题

--- src/gameboard.cpp
+++ src/gameboard.cpp
@@ -1,11 +1,60 @@
 #include "gameboard.h"
 #include "mario.h"
 #include "blocks.h"
+#include "coins.h"
 #include <QTimer>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+using std::fstream;
+using std::string;
 
 GameBoard::GameBoard(QObject *parent) : QObject(parent)
 {
-	timerId = startTimer(15);//开启一个每隔15ms触发一次的计时器，timerId是该计时器的名称，
+	timerId = startTimer(15);	//开启一个每隔15ms触发一次的计时器，timerId是该计时器的名称，
+}
+
+void GameBoard::setItems(const QString file)
+{
+	fstream in(":informations.txt");
+	string str;
+	while(getline(in, str))
+	{
+		int x = atoi(const_cast<char*>(str.substr(3, 7).c_str()));
+		int y = atoi(const_cast<char*>(str.substr(10, 7).c_str()));
+		
+		if(str.substr(0, 3) == "BLO") 
+		{		
+			blocks* block = new blocks(":picture/block.png");
+			block->setPos(x, y);
+			blocklist.push_back(block);
+		}
+		
+		else if(str.substr(0, 3) == "COI") 
+		{
+			coins* coin = new coins(":picture.coin.png");
+			coin->setPos(x, y);
+			coinlist.push_back(coin);
+		}
+		else if(str.substr(0, 3) == "MON")
+		{
+			
+		}
+		else if(str.substr(0, 3) == "FLA")
+		{
+			
+		}
+		else if(str.substr(0, 3) == "MUS")
+		{
+			
+		}
+		else if(str.substr(0, 3) == "TUR")
+		{
+			
+		}
+	}
+
 }
 
 void GameBoard::timerEvent(QTimerEvent *event)
