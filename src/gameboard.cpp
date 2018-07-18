292cf19fc3ac2c9a7cb297cf198cbb4368e06624
Tudero Li <33458031+Youggls@users.noreply.github.com>
2018-07-18 16:13:22 +0800

    已实现碰撞判断

--- src/gameboard.cpp
+++ src/gameboard.cpp
@@ -8,23 +8,88 @@ GameBoard::GameBoard(QObject *parent) : QObject(parent)
 	timerId = startTimer(15);//开启一个每隔15ms触发一次的计时器，timerId是该计时器的名称，
 }
 
-void GameBoard::setBlock(const QString file, qreal pos_x, qreal pos_y) {
-
-}
-
-void GameBoard::timerEvent(QTimerEvent *event) {
+void GameBoard::timerEvent(QTimerEvent *event)
+{
 	moveView();
 	moveMario();
 }
 
-void GameBoard::moveView() {
+void GameBoard::moveView()
+{
 	back->move();
-	for(int i = 0; i < blocklist.size(); i++) {
+	for(int i = 0; i < blocklist.size(); i++)
+	{
 		blocklist[i]->move();
 	}
 }
 
-void GameBoard::moveMario() {
+void GameBoard::moveMario()
+{
 	player->move();
 }
 
+bool GameBoard::isLeftcollider()
+{
+	QList<QGraphicsItem*> list = player->collidingItems();
+	if(list.isEmpty()) return false;
+	else
+	{
+		for(int i = 0; i < list.size(); i++)
+		{
+			if(list[i]->x() <= player->x() &&
+			   list[i]->y() <= player->y())
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+}
+
+bool GameBoard::isRightcollider()
+{
+	QList<QGraphicsItem*> list = player->collidingItems();
+	if(list.isEmpty()) return false;
+	else
+	{
+		for(int i = 0; i < list.size(); i++)
+		{
+			if(list[i]->x() >= player->x() &&
+			   list[i]->y() <= player->y())
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+}
+
+bool GameBoard::isUpcollider()
+{
+	QList<QGraphicsItem*> list = player->collidingItems();
+	if(list.isEmpty()) return false;
+	else
+	{
+		for(int i = 0; i < list.size(); i++)
+		{
+			if(list[i]->y() <= player->y())
+				return true;
+		}
+		return false;
+	}
+}
+
+bool GameBoard::isDowncollider()
+{
+	QList<QGraphicsItem*> list = player->collidesItem();
+	if(list.isEmpty()) return false;
+	else
+	{
+		for(int i = 0; i < list.size(); i++)
+		{
+			if(list[i]->y() >= player->y())
+				return true;
+		}
+		return false;
+	}
+}
